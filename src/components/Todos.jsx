110514087950e21c8ce84f8bf056183ec0e6936a
Aleksandr-Glebik <glebikaa@mail.ru>
2022-10-24 23:07:29 +0300

    feat: add animation - Reorder.Group and Reorder.Item, setting animation and add onRemove on doubleClick

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -1,19 +1,51 @@
 import React from "react"
+import {Reorder, AnimatePresence} from 'framer-motion'
 
-export const Todos = ( { todos = [] }) => {
+const variants = {
+    initial: {
+        opacity: 0,
+        height: 0
+    },
+    animate: {
+        opacity: 1,
+        height: 'auto'
+    },
+    exit: {
+        opacity: 0,
+        height: 0
+    }
+}
+
+export const Todos = ( { todos = [], setTodos, onRemove }) => {
     return (
-        <ol>
-            {todos.map( (todo) => (
-                <TodoItem key={todo.id} todo={todo} />
-            ))}
-        </ol>
+        <Reorder.Group as="ol" axys="y"
+                       values={todos}
+                       onReorder={setTodos}
+        >
+            <AnimatePresence initial={false} >
+                {todos.map( (todo) => (
+                    <TodoItem key={todo.id}
+                              todo={todo}
+                              onRemove={onRemove}
+                    />
+                ))}
+            </AnimatePresence>
+        </Reorder.Group>
     )
 }
 
-const TodoItem = ({ todo }) => {
+const TodoItem = ({ todo, onRemove }) => {
     return (
-        <li>
+        <Reorder.Item
+          value={todo}
+          whileDrag={{
+            scale: 1.035,
+            boxShadow: '5px 5px 1px 1px rgba(0, 0, 255, .1)'
+          }}
+          onDoubleClick={() => onRemove(todo.id)}
+          {...variants}
+        >
             <span>{todo.title}</span>
-        </li>
+        </Reorder.Item>
     )
 }
