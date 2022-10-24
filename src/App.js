9248f58a3499dd126de39f6a072eefbcc6b927b9
Aleksandr-Glebik <glebikaa@mail.ru>
2022-10-25 00:34:42 +0300

    feat: add animation for header elements: img, h1, p, btn = on scroll event

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import './App.css'
 import TheHeader from './components/TheHeader'
+import {useTransform, motion, useViewportScroll} from 'framer-motion'
 
 
 function App() {
+  const {scrollY} = useViewportScroll()
+  const offsetY = [0, 300]
+  const marginTop = useTransform(scrollY, offsetY, offsetY)
+
    return (
     <div style={{position: 'relative'}}>
-      <TheHeader />
-      <div className="section" style={{backgroundColor: 'maroon'}}>
+      <TheHeader offsetY={offsetY} scrollY={scrollY}/>
+      <motion.div className="section"
+                  style={{backgroundColor: 'maroon', marginTop}}
+      >
         <h1>Hello StackBlitz!</h1>
         <p>Start edition to see some magic happen :)</p>
-      </div>
+      </motion.div>
       <div className="section" style={{backgroundColor: 'peru'}}>
         Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words,
       </div>
--- src/components/TheHeader.jsx
+++ src/components/TheHeader.jsx
@@ -1,43 +1,62 @@
 import React from "react"
+import {motion, useTransform} from 'framer-motion'
 
-const TheHeader = () => {
-    const height = 250
-    const imgWidth = 100
-    const fontSize = '40px'
+const TheHeader = ({offsetY, scrollY}) => {
+    const heightSizes = [250, 50]
+    const imageSize = [100, 35]
+    const fontSizes = ['40px', '20px']
+
+    const height = useTransform(scrollY, offsetY, heightSizes)
+    const imgWidth = useTransform(scrollY, offsetY, imageSize)
+    const fontSize = useTransform(scrollY, offsetY, fontSizes)
+    const pOpacity = useTransform(scrollY, [20, 150], [1, 0])
+    const btnOpacity = useTransform(scrollY, [145, 300], [0, 1])
 
     return (
-        <header
+        <motion.header
             className="header"
             style={{
                 height
             }}
         >
             <div className="flex">
-                <img src="https://images.unsplash.com/photo-1620288627223-53302f4e8c74?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80" alt="logo" style={{width: imgWidth}} />
-                <h2
+                <motion.img src="https://images.unsplash.com/photo-1620288627223-53302f4e8c74?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80" alt="logo" style={{width: imgWidth}} />
+                <motion.h2
                     style={{
                         fontSize
                     }}
                 >
                     Hello Coders!
-                </h2>
+                </motion.h2>
+                <motion.button
+                    style={{
+                        marginLeft: 'auto',
+                        padding: '0.5rem 1rem',
+                        opacity: btnOpacity,
+                    }}
+                >
+                    Click me!
+                </motion.button>
             </div>
-            <div
+            <motion.p
                 style={{
-                    fontSize: 16
+                    fontSize: 18,
+                    marginTop: '15px',
+                    opacity: pOpacity
                 }}
             >
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid quae, voluptatum ipsa vel magni aliquam inventore deleniti accusantium.
-            </div>
-            <button
-                style={{
-                    margin: '1rem 0 0',
-                    padding: '0.5rem 1rem',
-                }}
-            >
-                Click me!
-            </button>
-        </header>
+                <button
+                    style={{
+                        display: 'block',
+                        margin: '1rem 0 0',
+                        padding: '0.5rem 1rem',
+                    }}
+                >
+                    Click me!
+                </button>
+            </motion.p>
+        </motion.header>
     )
 }
 
