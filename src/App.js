08b31b5ca49b9741cc20f0dd13c3d31b553e10e6
Aleksandr-Glebik <glebikaa@mail.ru>
2022-10-26 12:33:43 +0300

    feat: add animation for h2 on click btn, add useRef and turn off animation on first loadin

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
 import './App.css'
+import React, { useState, useRef } from 'react'
+import { Spring, animated } from 'react-spring'
 
 
 
 function App() {
+  const [isVisible, setIsVisible] = useState(false)
+  const myRef = useRef(null)
+
     return (
     <div className='App'>
       <h1>Hello React Spring - animation</h1>
-      <h2>Start learn this libraries</h2>
+      <button
+        onClick={() => setIsVisible(!isVisible)}
+      >
+        Click me!
+      </button>
+      <Spring
+        immediate={!myRef.current}
+        reset={true}
+        from={{opacity: 0, transform: 'translateY(-2rem)'}}
+        to={{opacity: 1, transform: 'translateY(0rem)'}}
+        config={{duration: 1000}}
+        reverse={!isVisible}
+      >
+        {(props) => (<animated.h2 style={props} ref={myRef}>Start learn this libraries</animated.h2>)}
+      </Spring>
     </div>
   )
 }
