b72a3655ba1bd9aef1366b18c014d80a257f8c61
Aleksandr-Glebik <glebikaa@mail.ru>
2022-10-26 13:46:41 +0300

    feat: add animation - rotate 2 carts on hook useSpring

--- src/App.js
+++ src/App.js
@@ -1,45 +1,32 @@
 import './App.css'
-import React, { useState, useRef } from 'react'
-import { Spring, Transition, animated } from 'react-spring'
-
-
+import React, { useState, } from 'react'
+import { useSpring, animated, config } from 'react-spring'
 
 function App() {
-  const [isVisible, setIsVisible] = useState(false)
-  const myRef = useRef(null)
+  const [flipped, setFlipped] = useState(false)
 
-    return (
-    <div className='App'>
-      <h1>Hello React Spring - animation</h1>
-      <button
-        onClick={() => setIsVisible(!isVisible)}
-      >
-        Click me!
-      </button>
-      <Spring
-        immediate={!myRef.current}
-        reset={true}
-        from={{opacity: 0, transform: 'translateY(-2rem)'}}
-        to={{opacity: 1, transform: 'translateY(0rem)'}}
-        config={{duration: 1000}}
-        reverse={!isVisible}
-      >
-        {(props) => (<animated.h2 style={props} ref={myRef}>Start learn this libraries</animated.h2>)}
-      </Spring>
+  const {opacity, transform} = useSpring({
+    opacity: flipped ? 1 : 0,
+    transform: `perspective(600px) rotateX(${flipped ? 180 : 0}deg)`,
+    config: config.gentle
+  })
 
-      <Transition
-        items={isVisible}
-        enter={{opacity: 1, transform: 'translateY(0rem)'}}
-        leave={{opacity: 0, transform: 'translateY(-2rem)'}}
-        from={{opacity: 0, transform: 'translateY(-2rem)'}}
-        config={{duration: 1000}}
-        reverse={!isVisible}
-      >
-        {(props, item) =>
-        item &&
-          <animated.h2 style={props}>Start learn this libraries</animated.h2>
-        }
-      </Transition>
+  return (
+    <div onClick={() => setFlipped(!flipped)}>
+      <animated.div
+        style={{
+          opacity: opacity.interpolate(o => 1 - o),
+          transform,
+        }}
+        className={'c back'}
+      />
+      <animated.div
+        style={{
+          opacity,
+          transform: transform.interpolate(t => `${t} rotateX(180deg)`),
+        }}
+        className={'c front'}
+      />
     </div>
   )
 }
