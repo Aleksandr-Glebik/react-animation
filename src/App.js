438df8fb7a32f1d0f9da6112b3e13273f3f9aff1
Aleksandr-Glebik <glebikaa@mail.ru>
2022-10-26 11:55:43 +0300

    feat: start learne react-spring animation libraries, add min markup and style

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
 import './App.css'
-import TheHeader from './components/TheHeader'
-import {useTransform, motion, useViewportScroll} from 'framer-motion'
+
 
 
 function App() {
-  const {scrollY} = useViewportScroll()
-  const offsetY = [0, 300]
-  const marginTop = useTransform(scrollY, offsetY, offsetY)
-
-   return (
-    <div style={{position: 'relative'}}>
-      <TheHeader offsetY={offsetY} scrollY={scrollY}/>
-      <motion.div className="section"
-                  style={{backgroundColor: 'maroon', marginTop}}
-      >
-        <h1>Hello StackBlitz!</h1>
-        <p>Start edition to see some magic happen :)</p>
-      </motion.div>
-      <div className="section" style={{backgroundColor: 'peru'}}>
-        Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words,
-      </div>
+    return (
+    <div className='App'>
+      <h1>Hello React Spring - animation</h1>
+      <h2>Start learn this libraries</h2>
     </div>
   )
 }
